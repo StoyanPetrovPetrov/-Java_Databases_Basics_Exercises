a473daa8392773689967c241872375b4df0e8f78
StoyanPetrovPetrov <104095552+StoyanPetrovPetrov@users.noreply.github.com>
2022-10-01 17:57:33 +0300

    Create 06UniversityDatabase.sql

---
+++ Table Relations - Exercise/06UniversityDatabase.sql
@@ -0,0 +1,49 @@
+CREATE TABLE `subjects`(
+`subject_id`INT(11) PRIMARY KEY AUTO_INCREMENT,
+`subject_name`VARCHAR(50) 
+);
+CREATE TABLE `majors`(
+`major_id`INT(11)PRIMARY KEY AUTO_INCREMENT,
+`name`VARCHAR(50)
+);
+CREATE TABLE `payments`(
+`payment_id`INT(11) PRIMARY KEY AUTO_INCREMENT,
+`payment_date`DATE ,
+`payment_amount`DECIMAL(8,2),
+`student_id`INT(11)
+);
+CREATE TABLE `students`(
+`student_id`INT(11)PRIMARY KEY AUTO_INCREMENT,
+`student_number`VARCHAR(12)  UNIQUE,
+`student_name`VARCHAR(50),
+`major_id`INT(11)
+);
+
+CREATE TABLE `agenda`(
+`student_id`INT,
+`subject_id`INT,
+CONSTRAINT pk
+PRIMARY KEY(`student_id`,`subject_id`)
+);
+
+ALTER TABLE `payments`
+ADD CONSTRAINT fk_students
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`);
+
+ALTER TABLE `students`
+ADD CONSTRAINT fk_majors
+FOREIGN KEY (`major_id`)
+REFERENCES `majors`(`major_id`);
+
+ALTER TABLE `agenda`
+ADD CONSTRAINT fk_subjects
+FOREIGN KEY (`subject_id`)
+REFERENCES `subjects`(`subject_id`);
+
+ALTER TABLE `agenda`
+ADD CONSTRAINT fk_students_agenda
+FOREIGN KEY (`student_id`)
+REFERENCES `students`(`student_id`);
+
+
